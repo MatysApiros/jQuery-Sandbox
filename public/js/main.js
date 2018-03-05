64fe9d4db7443c7d911642d6d8aff3b641f3defc
MatysApiros <matheus.canabarro@hotmail.com>
2018-03-05 10:44:55 -0300

    Added count function for text entered in the text area

--- public/js/main.js
+++ public/js/main.js
@@ -3,3 +3,17 @@ var numeroDePlavras = frase.split(" ").length;
 var tamanhoFrase = $("#tamanhoFrase");
 
 tamanhoFrase.text(numeroDePlavras);
+
+var campo = $(".campo-digitacao");
+campo.on("input", function(){
+    var conteudo      = campo   .val();
+    var qtdPalavras   = conteudo.split(/\s+/).length - 1;
+    var qtdCaracteres = conteudo.length;
+
+    $("#contador-palavras")  .text(qtdPalavras);
+    $("#contador-caracteres").text(qtdCaracteres);
+});
+
+campo.on("focus", function(){
+    console.log("Estou digitando!");
+});
